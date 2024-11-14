8e6bc28b8c6f5466b699c6cbf8cb823fef8d77c0
kimdanbin <rlaeksqls@naver.com>
2024-11-15 04:37:10 +0900

    init commit

---
+++ src/main/java/com/example/spartaschedulejpa/SpartaScheduleJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartaschedulejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaScheduleJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaScheduleJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spartaschedulejpa/SpartaScheduleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartaschedulejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaScheduleJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
